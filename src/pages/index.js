57c2aa63b4c9993a0b13538eab01690d899f8da2
Margarita Villaseñor <maggie.villasenor@hotmail.com>
2023-06-03 00:46:31 -0700

    ci: enabled access from the home page to the product catalog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1>Home Page</h1>
+import { useRouter } from "next/router";
+import { Button } from "@mui/material";
+import styles from "../styles/Home.module.css";
+
+export default function Home({title, summary}) {
+  const router = useRouter();
+
+  return  (
+    <section className={styles.Home}>
+      <h1 className={styles.Name}>{title}</h1>
+      <div className={styles.Summary}>{summary}</div>
+      <div>
+        <Button variant="outlined" color="secondary" size="large" onClick={() => router.push("/catalog")}>Shop Now</Button>
+      </div>
+    </section>
+  );
+}
+
+export async function getStaticProps () {
+  //Get your data from an API
+  return {
+    props: {
+      title: "Welcome to Ma Chérie",
+      summary: "Elevate your style with Ma Chérie's timeless elegance and refined fashion. Experience high couture designs that embody grace and glamour"
+    }
+  }
 }
